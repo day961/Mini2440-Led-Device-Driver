f05f36cbc479e73b0d783480351db2ed15db3dae
Svoday <day961@163.com>
2013-04-08 23:10:43 +0800

    优化可读性

--- led.c
+++ led.c
@@ -1,5 +1,5 @@
 /*
- *led.c mini2440led驱动
+ *led.c mini2437led驱动
  */
 #include<linux/module.h>
 #include<linux/init.h>
@@ -22,53 +22,46 @@
 #define GPBDAT	0X56000014
 #define GPBUP	0X56000018
 /*存放寄存器相对地址*/
-unsigned short	*rGPBCON;
-unsigned char	*rGPBDAT;
-unsigned char	*rGPBUP;
+unsigned volatile short	*rGPBCON;
+unsigned volatile char	*rGPBDAT;
+unsigned volatile char	*rGPBUP;
 
 dev_t led_num;//存放设备号
 struct cdev *my_led;//创建cdev结构  字符设备结构体
 int major=0,minor=0;
 static struct class *led_class;
 //static struct class_device *led_class_dev;
+/*函数声明*/
+void led_init(void);
 
 
 /***************打开实现函数*********************************/
 static int led_open(struct inode *inode,struct file *file)
 {
+//	led_init();//初始化led，包括内存映射，io初始化
 	printk("led device open\n");
 	return 0;
 }
+
+/***************关闭实现函数*********************************/
+static int led_release(struct inode *inode,struct file *file)
+{
+	printk("led device close\n");
+	return 0;
+}
+
 /**************IOctrl实现函数******************************/
 /*
 cmd:led控制命令 0关1开
 arg:led选择	led0~3
- */
+*/
 static int led_ioctl(struct inode *inode,struct file *file,unsigned int cmd,unsigned long arg)
 {
 	switch(cmd)
 	{
 		case 0:
 		case 1:
-	/*		if(arg > 4)
-			{       
-				return -EINVAL;
-			}//检查led号
-				switch(arg)
-				{
-					case 0:
-					value_dat |= 0x2;
-					iowrite16(value_dat,rGPBDAT);
-					return 0;
-					break;
-					case 1:
-					value_dat &= (~0x1E0);
-					iowrite16(value_dat,rGPBDAT);
-					return 0;
-					break;
-				}
-	*/
-			setled(cmd,arg);
+			setled(cmd,arg);//进入设置led状态函数
 			break;
 			return 0;
 		default: 
@@ -83,37 +76,8 @@ static struct file_operations myled_fops =
 	.owner = THIS_MODULE, 
 	.open  = led_open,
 	.ioctl = led_ioctl,
+	.release = led_release,
 };
-/*******************************************************/
-
-void led_init(void)
-{
-	unsigned int 	value_con;	//别用short!!!!!
-	unsigned int	value_up;
-	unsigned int	value_dat;
-				
-	rGPBCON	= ioremap(GPBCON , 4);
-	rGPBDAT	= ioremap(GPBDAT , 2);
-	rGPBUP	= ioremap(GPBUP  , 2);
-
-	value_con = ioread32(rGPBCON);//读取内存
-	value_con = (value_con&(~(0xff<<10)))|(0x15400); //0x3fc00 = 0011,1111,1100,0000,0000. 0x15400 = 0001,0101,0100,0000,0000
-	iowrite32(value_con,rGPBCON);//写回内存 记得写够32位！！被坑了！！
-
-	value_up = ioread16(rGPBUP);
-	value_up |= 0x1e0;	//0x1e0 = 0001,1110,0000
-	iowrite16(value_up,rGPBUP);
-	
-	value_dat = ioread16(rGPBDAT);
-	value_dat |= 0x1e0;	//全部熄灭
-	iowrite16(value_dat,rGPBDAT);
-
-	printk("led init complete\n");
-
-//	s3c2410_gpio_cfgpin(S3C2410_GPB(8),S3C2410_GPIO_OUTPUT);
-//	s3c2410_gpio_setpin(S3C2410_GPB(8),0);
-}
-
 /**__init函数**/
 static int __init dev_init(void)
 {
@@ -121,26 +85,25 @@ static int __init dev_init(void)
 	result = alloc_chrdev_region(&led_num,0,1,"ledtest");//动态申请设备号
 	if(result<0){
 		printk(KERN_WARNING "led:can't get major");	
-		return result;//检查是否申请成功
+		return result;//返回错误值
 	}
 
 	/*字符设备的注册*/
 	my_led=cdev_alloc();//分配空间给my_cdev
 	cdev_init(my_led,&myled_fops);//初始化已分配到的结构
 	my_led->ops=&myled_fops;//指定file operations
-	my_led->owner=THIS_MODULE;
-	if(cdev_add(my_led,led_num,1))//将cdev添加如内核
+	my_led->owner=THIS_MODULE;//指定模块拥有者
+	if(cdev_add(my_led,led_num,1))//将cdev添加入内核
 		printk(KERN_NOTICE"Someting wrong when adding cdev\n");
 	else
 		printk("Success adding cdev\n");
 
-
 	led_init();//初始化led，包括内存映射，io初始化
-
 
 	/*用于在/dev创建设备节点*/
 	led_class = class_create(THIS_MODULE,"ledtest");//创建设备类
 	device_create(led_class,NULL,led_num,NULL,"ledtest");//创建设备节点
+	
 	return 0;
 }
 
--- led.h
+++ led.h
@@ -1,6 +1,13 @@
-#define GPBDAT  0X56000014
-extern unsigned char   *rGPBDAT;
+#define GPBCON	0X56000010
+#define GPBDAT	0X56000014
+#define GPBUP	0X56000018
+
+extern unsigned volatile short	*rGPBCON;
+extern unsigned volatile char   *rGPBDAT;
+extern unsigned volatile char	*rGPBUP;
+
 int setled(unsigned int cmd,unsigned long arg);
+void led_init(void);
 
 int setled(unsigned int cmd,unsigned long arg)
 {
@@ -56,3 +63,29 @@ int setled(unsigned int cmd,unsigned long arg)
 	}
 	return 0;
 }
+
+void led_init(void)
+{
+	unsigned int 	value_con;	//别用short!!!!!
+	unsigned int	value_up;
+	unsigned int	value_dat;
+				
+	rGPBCON	= ioremap(GPBCON , 4);
+	rGPBDAT	= ioremap(GPBDAT , 2);
+	rGPBUP	= ioremap(GPBUP  , 2);
+
+	value_con = ioread32(rGPBCON);//读取内存
+	value_con = (value_con&(~(0xff<<10)))|(0x15400); //0x3fc00 = 0011,1111,1100,0000,0000. 0x15400 = 0001,0101,0100,0000,0000
+	iowrite32(value_con,rGPBCON);//写回内存 记得写够32位！！被坑了！！
+
+	value_up = ioread16(rGPBUP);
+	value_up |= 0x1e0;	//0x1e0 = 0001,1110,0000
+	iowrite16(value_up,rGPBUP);
+	
+	value_dat = ioread16(rGPBDAT);
+	value_dat |= 0x1e0;	//全部熄灭
+	iowrite16(value_dat,rGPBDAT);
+
+	printk("led init complete\n");
+}
+
